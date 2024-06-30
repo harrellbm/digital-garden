fdc5ced6b0f17b0cf9d0ac141b6c089e1cf73096
harrellbm <brendenharrell1@gmail.com>
2024-06-30 14:37:53 -0500

    basic config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌱 Pastor Harrell's 🪴 Digital Garden 🌳",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
