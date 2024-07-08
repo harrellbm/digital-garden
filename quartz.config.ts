db932b560f833e500735a448cd6450cc965dbbbb
B Harrell <55707292+harrellbm@users.noreply.github.com>
2024-07-08 16:01:45 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       typography: {
         header: "Special Elite",
         body: "Architects Daughter",
-        code: "Amatic SC Bold",
+        code: "Amatic SC",
       },
       colors: {
         lightMode: {
