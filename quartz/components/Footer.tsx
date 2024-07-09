b07fd0a0d442caab7a5b22e64beb475abed26556
harrellbm <brendenharrell1@gmail.com>
2024-07-09 15:13:06 -0500

    updated footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,8 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          I would love to hear what you think about the ideas you find in this garden. You can find me with the linked contact information below:
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
@@ -25,6 +24,9 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
+        <p>
+          Created by Brenden Harrell using <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> and a little DIY coding. 
+        </p>
       </footer>
     )
   }
